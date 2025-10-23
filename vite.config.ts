fab4ae1393a846c30f1984c43c06b94a59ad7a6e
adamk <adamkc2001@gmail.com>
2025-10-23 14:08:56 +0200

    Setup GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Groningen_Tour/", // 👈 add this line
+  base: "/Groningen_Tour/",
   plugins: [react()],
 });
